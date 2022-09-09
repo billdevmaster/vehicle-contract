64d54f87d9b6b705cb368eacc90674da60eb5524
billdevmaster71 <billdevmaster@gmail.com>
2022-09-09 10:00:22 +0300

    feat: add rinkeby, mumbai network

--- truffle-config.js
+++ truffle-config.js
@@ -10,9 +10,21 @@ module.exports = {
       port: 8545,            // Standard BSC port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    ropesten: {
+    ropsten: {
       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${process.env.ROPSTEN_INFURA_API_KEY}`),
       network_id: 3,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`),
+      network_id: 4,       // Rinkeby's id
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true  ,   // Skip dry run before migrations? (default: false for public nets )
+      networkCheckTimeout: 100000,
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      network_id: 80001,
     },
   },
 
@@ -20,12 +32,12 @@ module.exports = {
   compilers: {
     solc: {
       version: "^0.8.4", // A version or constraint - Ex. "^0.5.0"
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 1000   // Optimize for how many times you intend to run the code
-        }
-      }
+      // settings: {
+      //   optimizer: {
+      //     enabled: true,
+      //     runs: 1000   // Optimize for how many times you intend to run the code
+      //   }
+      // }
     },
   },
   plugins: [
@@ -35,6 +47,6 @@ module.exports = {
     // bscscan: 'QC4UPPIN4JPDA4YAY1PAS46RGNJUYFHKCM',
     // polygonscan: 'X1RVAXVNZFIXVMYUGS8DW8MR8NUZM5VA58'
     // bsctestscan: 'NPIT4183DK8BMGVZDT9C4R14S1QMEHIT88',
-    etherscan: 'QNH5TIIYA5GXN2TT326FFBMSSY3H5B24XX'
+    etherscan: 'AE1GZQSMZDCKH8WP4KZY1UPSNTD8KAN64K'
   },
 }
